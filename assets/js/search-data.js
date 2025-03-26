6746c817c7ed0b21a2dd403054e8f9b30f0070cc
rstao-bjtu <63637062+rstao-bjtu@users.noreply.github.com>
2025-03-26 08:08:05 +0000

    Deploying to gh-pages from @ rstao-bjtu/rstao-bjtu.github.io@2a8ceb7a4f893a35c7e65ffa3336982ef604d288 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -11,7 +11,7 @@ ninja.data = [{
     },
   },{id: "nav-publications",
           title: "Publications",
-          description: "",
+          description: "†Equal Contribution, *Corresponding Author",
           section: "Navigation",
           handler: () => {
             window.location.href = "/publications/";
