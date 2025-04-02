c22b14b4ad21de197b5f64df181dae40e26364af
rstao-bjtu <63637062+rstao-bjtu@users.noreply.github.com>
2025-04-02 05:59:46 +0000

    Deploying to gh-pages from @ rstao-bjtu/rstao-bjtu.github.io@26e05715be634f913e5d03e4f3f3efff8345ef5f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,6 +23,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/people/";
           },
+        },{id: "nav-statement",
+          title: "Statement",
+          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/cv/";
+          },
         },{id: "post-a-post-with-image-galleries",
         
           title: "a post with image galleries",
