fda6456eac9f9720ad70900dfaad3556fe8057ac
rstao-bjtu <63637062+rstao-bjtu@users.noreply.github.com>
2025-06-10 04:58:37 +0000

    Deploying to gh-pages from @ rstao-bjtu/rstao-bjtu.github.io@2ffe5666515bb10afeee1a563feaed00c1edb59f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -4,7 +4,7 @@ const ninja = document.querySelector('ninja-keys');
 // add the home and posts menu items
 ninja.data = [{
     id: "nav-about",
-    title: "About",
+    title: "about",
     section: "Navigation",
     handler: () => {
       window.location.href = "/";
