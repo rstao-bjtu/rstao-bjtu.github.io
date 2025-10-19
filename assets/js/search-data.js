612cc7c0605db3ade33ec0b4dc78963ed32a10de
rstao-bjtu <63637062+rstao-bjtu@users.noreply.github.com>
2025-10-19 11:40:01 +0000

    Deploying to gh-pages from @ rstao-bjtu/rstao-bjtu.github.io@74b1e186411d8ce09e0b2247d5bc9ecdbea09c12 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -32,13 +32,6 @@ ninja.data = [{
           },
         },{id: "nav-gathering",
           title: "Gathering",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/blog/";
-          },
-        },{id: "nav-gathering",
-          title: "Gathering",
           description: "Materials for courses you taught. Replace this text with your description.",
           section: "Navigation",
           handler: () => {
